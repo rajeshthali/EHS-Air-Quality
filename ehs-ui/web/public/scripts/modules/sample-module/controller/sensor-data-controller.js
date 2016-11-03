ce9b031c5bd56c6d7e7e5b89831d170a26d1c55d
Rajesh Thali <Rajesh.Thali@gmail.com>
2016-11-03 11:30:06 +0530

    Rajesh Thali 20161103 EHS changes

--- ehs-ui/web/public/scripts/modules/sample-module/controller/sensor-data-controller.js
+++ ehs-ui/web/public/scripts/modules/sample-module/controller/sensor-data-controller.js
@@ -4,12 +4,11 @@ define([ 'angular', './controllers-module'], function(angular, controllers) {
 			function($scope, $http, $state, $log, $interval, $rootScope, AuthService, SensorDataService) {
 		
 		var intervalPromiseSensor = null;
-		$scope.sensorData = null;
 		var sensorCharts = [];
 		var startDynamiUpdate = function() {
 			intervalPromiseSensor = $interval(function() {
 				realSensorData();
-			}, 20000);
+			}, 40000);
 		};
 		$scope.$on('$destroy', function() {
 			$scope.stop();
@@ -29,57 +28,32 @@ define([ 'angular', './controllers-module'], function(angular, controllers) {
 		loadData();
 		 
 		var realSensorData = function() {
-			SensorDataService.loadSensorData($rootScope.token,20000, function(res){
-				   $scope.sensorDataList= angular.copy(res);
+			SensorDataService.loadSensorData($rootScope.token,40000, function(res){
+				   var sensorDataList= angular.copy(res);
+				   for (var i = 0; i < sensorDataList.length; i++) {
+					   var sensorName = sensorDataList[i].name;
+					   var sensorDataValues = sensorDataList[i].sensorDataValues;
+					   var dataXaxis =[];
+					   var dataYaxis =[];
+					   for (var j = 0; j < sensorDataValues.length; j++) {
+						   dataXaxis.push(sensorDataValues[j].timeStamp);
+						   dataYaxis.push(sensorDataValues[j].sensorValue);
+					   }
+					   
+					   setTimeout(function() {
+							$('.aqi_details_graph_class').fadeIn();
+							loadValuesToGraph('Container_'+sensorName,convertTimeStamps(dataXaxis),dataYaxis,sensorName);
+						}, 300);
+				
+					   
+				   }
+				 
+				   //  var series = getSeries($scope.data);
+			   });
 				   
-				  // Console.log("Test");
-				   /*for(var i=0; i< res.length;i++) {
-				  sensorData.name = res[i].name;
-					  var valueList =  res[i].sensorDataValues;
-					  
-				   }*/
-				   
-	    		  // $scope.len = res.length;
-			       //console.log("length of the data is :" +$scope.len);
-			       for (var i = 0; i < sensorDataList.length; i++) {
-			    	    //$scope.sensor = null;
-			    	    var data = sensorDataList[i];
-			    	    $scope.sensor[i] = data;
-			    	    $scope.slen = $scope.sensor.length;
-			    	    console.log("data in the sensor:" + $scope.slen);
-			    	    data.temperature = [];
-			    	    $scopedata.PB = [];
-						//data.O3 = [];
-					    //data.CO2 = [];
-						//data.PM2_5 = [];
-						//data.NH3 = [];
-						//data.PM10 = [];
-						//data.SO2 = [];
-					   // data.timestamp = [];
-				        for (var j = 0; j < $scope.slen; j++) {
-						 
-						$scope.sensor= [];
-						$scope.data.temperature.push(sensorDataList[j].temperature);
-						$scope.data.PB.push(sensorDataList[j].PB);
-						$scope.data.O3.push(sensorDataList[j].O3);
-						$scope.data.CO2.push(sensorDataList[j].CO2);
-						$scope.data.PM2_5.push(sensorDataList[j].PM2_5);
-						$scope.data.NH3.push(sensorDataList[j].NH3);
-						$scope.data.PM10.push(sensorDataList[j].PM10);
-						$scope.data.SO2.push(sensorDataList[j].SO2);
-						$scope.data.timestamp.push(sensorDataList[j].timestamp);
-						
-					
-					}$scope.sensor= [];
-					
-					 var maxIndex = getMaxIndex($scope.data);
-				     var series = getSeries($scope.data);
-			   }
-				   
-			   loadValuesToGraph('container');
+			   
 			
-          });
-		 }
+       	 }
 		   var getMaxIndex = function(data) {
 				var big = 0;
 				var index = 0;
@@ -206,7 +180,7 @@ define([ 'angular', './controllers-module'], function(angular, controllers) {
 
 			
 			
-			var loadValuesToGraph = function(id, dataX, dataY, index) {
+			var loadValuesToGraph = function(id, dataX, dataY, sensorName) {
 				// console.log(id + ' >> ' + tabIndex);
 				$(id).each(function() {
 					// console.log('each');
@@ -222,7 +196,7 @@ define([ 'angular', './controllers-module'], function(angular, controllers) {
 						},
 						tooltip : {
 							formatter : function() {
-								return '<b>' + this.series.name + '</b><br/>' + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' + Highcharts.numberFormat(this.y, 2);
+								return '<b>' + sensorName + '</b><br/>' + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' + Highcharts.numberFormat(this.y, 2);
 							}
 						},
 						exporting : {
